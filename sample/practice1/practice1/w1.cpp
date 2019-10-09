96fb0a139a9724e99361de0dd268c4260bb02d6f
Jin-pyo <48873560+Jin-pyo@users.noreply.github.com>
2019-10-08 21:21:53 -0400

    Function pointer
    example of function pointer

--- sample/practice1/practice1/w1.cpp
+++ sample/practice1/practice1/w1.cpp
@@ -1,13 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<string>
 
-#include <iostream>
-#include"Text.h"
 
-int main() {
-	Array<> s, t;
-	Array<double> u;
-	Array<int, 40> v;
+int sum(int a, int b)
+{
+	return a + b;
+}
 
-	std::cout << Array<>::cnt() << std::endl;
-	std::cout << Array<double>::cnt() << std::endl;
-	std::cout << Array<int, 40>::cnt() << std::endl;
+int sub(int a, int b)
+{
+	return a - b;
+}
+
+int multiple(int a, int b)
+{
+	return a * b;
+}
+
+int main()
+{
+	int a=2, b=6;
+	int(*fncptr[3])(int, int) = { sum,sub,multiple };
+	
+	for (int i = 0; i < sizeof(fncptr) / sizeof(fncptr[0]); i++)
+	{
+		std::cout << fncptr[i](a, b) << std::endl;
+	}
+	return 0;
 }
