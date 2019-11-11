57ac7983a49760ea6ddc5f5095bb57a71b2598ac
Jin-pyo <48873560+Jin-pyo@users.noreply.github.com>
2019-11-11 14:08:57 -0500

    w8_start

---
+++ w8_athome/w8_athome/Utilities.cpp
@@ -0,0 +1,18 @@
+#include <memory>
+#include "List.h"
+#include "Element.h"
+#include "Utilities.h"
+
+using namespace std;
+
+namespace sdds {
+	List<Product> mergeRaw(const List<Description>& desc, const List<Price>& price) {
+		List<Product> priceList;
+		// TODO: Add your code here to build a list of products
+		//         using raw pointers
+
+
+
+		return priceList;
+	}
+}
